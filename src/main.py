1cc5088291c4d2d6c1bf94b91a860b9ed8e531cc
Petrakov Oleg <murami.ike@gmail.com>
2023-10-22 13:01:27 +0300

    Add proto schema

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
 import asyncio
+import base64
 import json
+
+import schema_pb2
 
 
 def event_handler(event, context):
     print('Event:', event)
+    if 'body' in event:
+        data = base64.b64decode(event['body'])
+        program_request = schema_pb2.NewProgramRequest()
+        program_request = program_request.ParseFromString(data)
+        print(program_request)
+
+        return {'status': 'OK'}
+    else:
+        return {'status': 'FAIL'}
